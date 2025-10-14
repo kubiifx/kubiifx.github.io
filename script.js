3a8005729e52a3e23ca7b0bf5969b8a27e62dce7
kubiifx <kubiifx@gmail.com>
2025-10-14 20:21:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const CORRECT_PASSWORD = "test123";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("loginBtn");
+  const input = document.getElementById("password");
+  const errorMsg = document.getElementById("error-msg");
+
+  btn.addEventListener("click", () => {
+    const userInput = input.value.trim().toLowerCase();
+
+    if (userInput === CORRECT_PASSWORD) {
+      window.location.href = "home.html";
+    } else {
+      errorMsg.textContent = "Špatné heslo. Zkus to znovu.";
+      input.value = "";
+      input.focus();
+    }
+  });
+
+  input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") btn.click();
+  });
+});
